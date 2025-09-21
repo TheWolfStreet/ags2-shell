edd42d82af4d2d1803cda84b82a84490edf5d08d
TheWolfStreet <wolfthestreet@gmail.com>
2025-09-21 04:28:18 +0300

    Fix style inconsistency Fix wallpaper not applying if the "background" file was deleted Fix swww overlaying gifs

--- service/wallpaper.ts
+++ service/wallpaper.ts
@@ -1,11 +1,10 @@
 import app from "ags/gtk4/app"
 import { createBinding } from "ags"
 import GObject, { property, register } from "ags/gobject"
-import { monitorFile } from "ags/file"
 import { execAsync } from "ags/process"
 
 import env from "$lib/env"
-import { bash, bashSync, dependencies } from "$lib/utils"
+import { bash, dependencies, fileExists } from "$lib/utils"
 
 @register()
 export default class Wallpaper extends GObject.Object {
@@ -23,11 +22,6 @@ export default class Wallpaper extends GObject.Object {
 
 		this.#apply()
 
-		monitorFile(this.wallpaper, () => {
-			this.notify("wallpaper")
-			this.#apply()
-		})
-
 		let prevMonCount = app.get_monitors().length
 		createBinding(app, "monitors").subscribe(() => {
 			const monCount = app.get_monitors().length
@@ -42,29 +36,39 @@ export default class Wallpaper extends GObject.Object {
 	async #handleHeic(imgPath: string) {
 		if (!dependencies("heif-dec")) return
 		const tmpImg = `${env.paths.tmp}/heic.png`
-		await bash(`heif-dec "${imgPath}" "${tmpImg}" && cp "${tmpImg}" "${`${env.paths.home}/.config/background`}"`)
+		await bash(`heif-dec "${imgPath}" "${tmpImg}" && cp "${tmpImg}" "${this.wallpaper}"`)
 	}
 
 	get_wallpaper() {
 		return this.wallpaper
 	}
 
-	async set_wallpaper(imgPath: string) {
-		if (!dependencies("swww")) return
-		const isHeic = imgPath.toLowerCase().endsWith(".heic")
+	async set_wallpaper(img_path: string) {
+		if (!dependencies("swww"))
+			return
+
+		const isHeic = img_path.toLowerCase().endsWith(".heic")
 
 		try {
 			if (isHeic) {
-				this.#handleHeic(imgPath)
+				await this.#handleHeic(img_path)
 			} else {
-				bashSync(`cp "${imgPath}" "${`${env.paths.home}/.config/background`}"`)
+				await bash(`cp "${img_path}" "${this.wallpaper}"`)
 			}
+			this.#apply()
 		} catch (e) {
 			console.error("Failed to set wallpaper:", e)
 		}
 	}
 
 	readonly #apply = async () => {
-		await execAsync(`swww img --invert-y --transition-type fade "${this.wallpaper}"`)
+		this.notify("wallpaper")
+
+		if (fileExists(this.wallpaper)) {
+			await execAsync(`swww clear-cache`)
+			await execAsync(`swww img --invert-y --transition-type fade "${this.wallpaper}"`)
+		} else {
+			await execAsync("swww clear 111111")
+		}
 	}
 }
